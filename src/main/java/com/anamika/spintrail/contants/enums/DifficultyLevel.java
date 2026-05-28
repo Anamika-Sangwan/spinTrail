0512550daa6c2f130f4347363bdf80ccd779826b
Anamika-Sangwan <mnianamika@gmail.com>
2026-05-28 21:30:59 +0530

    added Repo for Trail and waypoints

---
+++ src/main/java/com/anamika/spintrail/contants/enums/DifficultyLevel.java
@@ -0,0 +1,6 @@
+package com.anamika.spintrail.contants.enums;
+public interface DifficultyLevel{
+    String EASY = "EASY";
+    String MODERATE = "MODERATE";
+    String HARD = "HARD";
+}
--- src/main/java/com/anamika/spintrail/entity/Trail.java
+++ src/main/java/com/anamika/spintrail/entity/Trail.java
@@ -1,20 +1,47 @@
 package com.anamika.spintrail.entity;
 
+import com.anamika.spintrail.constants.enums.DifficultyLevel;
 import jakarta.persistence.*;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "trails")
 @Setter
+@Getter
 public class Trail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "trail_name", nullable = false)
     private String trailName;
 
+    @Column(nullable = false)
     private double distance;
 
+    @Column(columnDefinition = "geometry(LineString,4326)")
+    private LineString path;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DifficultyLevel difficulty; //EASY, MODERATE, HEIGHT
+
+    @Column
+    private String surfaceType; //"asphalt", "gravel", "mixed"
+
+    @Column
+    private Double elevationGain; // in meters
+
+    @Column(name = "created_at",updatable = false)
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "trail", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Waypoint> waypoints = new ArrayList<>();
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
     public Trail(){}
 
 }
